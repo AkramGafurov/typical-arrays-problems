b969bafd8f9b01e008f238662c212cd02d2a39a3
AkramGafurov <akramminsk@yander.ru>
2020-10-03 15:59:01 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+exports.min = function min(array) {
+    if (array !== undefined && array.length !== 0) {
+        let sortFunc = (a, b) => {
+            return a - b;
+        };
+        array.sort(sortFunc);
+        return array[0];
+    }
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array !== undefined && array.length !== 0) {
+        let sortFunc = (a, b) => {
+            return a - b;
+        };
+        array.sort(sortFunc);
+        return array[array.length - 1];
+    }
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array !== undefined && array.length !== 0) {
+        return (
+            array.reduce(function (a, b) {
+                return a + b;
+            }) / array.length
+        ).toFixed(2);
+    }
+    return 0;
+};
